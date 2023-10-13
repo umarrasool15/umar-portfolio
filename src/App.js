7e1b18b73a613e2b0a6ecc79e179382d60695d84
Umar Rasool <umarrasool15@gmail.com>
2023-10-13 13:47:15 -0400

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Profile from './Profile';
+// Import other components
+
+function App() {
+    return (
+        <div className="app">
+            <Header />
+            <Profile />
+            {/* Add other components */}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header">
+            <a href="/">umarrasool.dev</a>
+            {/* Add other header links or items */}
+        </div>
+    );
+}
+
+export default Header;
